24bd39f16f4a8cbc346d07dc96b98bb64d00512a
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-30 15:12:17 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/* Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità.
+1. Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
+2. Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+3. Creiamo poi una rotta /posts che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array locale.
+La rotta relativa ai post dovrà chiamare la funzione index() dal controller dedicato ( controllers/posts.js )
+4. Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+5. Testare nel browser. */
